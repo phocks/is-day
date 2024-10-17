c186cc596bd6bc4c2e02d80d11250ad043f31aa0
Joshua Byrd <phocks@gmail.com>
2024-10-17 15:09:22 +1000

    First version

--- mod.ts
+++ mod.ts
@@ -1,13 +1,40 @@
-export function isMonday(offsetHours: number = 10): boolean {
-  const MILLISECONDS_IN_MINUTE = 60000;
-  const MINUTES_IN_HOUR = 60;
+const MILLISECONDS_IN_MINUTE = 60000;
+const MINUTES_IN_HOUR = 60;
 
-  const now = new Date();
-  const offsetMinutes = offsetHours * MINUTES_IN_HOUR;
-  const localOffset = now.getTimezoneOffset();
+type Thing =
+  | "Monday"
+  | "Tuesday"
+  | "Wednesday"
+  | "Thursday"
+  | "Friday"
+  | "Saturday"
+  | "Sunday";
+
+interface Options {
+  offsetHours: number;
+  when: Date;
+}
+
+export function isit(
+  something: Thing,
+  options: Options = { offsetHours: 10, when: new Date() },
+): boolean {
+  enum Days {
+    Sunday = 0,
+    Monday = 1,
+    Tuesday = 2,
+    Wednesday = 3,
+    Thursday = 4,
+    Friday = 5,
+    Saturday = 6,
+  }
+
+  const offsetMinutes = options.offsetHours * MINUTES_IN_HOUR;
+  const localOffset = options.when.getTimezoneOffset();
   const offsetTime = new Date(
-    now.getTime() + (offsetMinutes + localOffset) * MILLISECONDS_IN_MINUTE,
+    options.when.getTime() +
+      (offsetMinutes + localOffset) * MILLISECONDS_IN_MINUTE,
   );
 
-  return offsetTime.getDay() === 1;
+  return offsetTime.getDay() === Days[something];
 }
--- mod_test.ts
+++ mod_test.ts
@@ -1,7 +1,60 @@
 import { assertEquals } from "@std/assert";
-import { isMonday } from "./mod.ts";
+import { isit } from "./mod.ts";
 
-Deno.test(function addTest() {
-  // assertEquals(add(2, 3), 5);
-  isMonday(10);
+Deno.test(function monday() {
+  assertEquals(
+    isit("Monday", { when: new Date("2024-10-14T00:00:00Z"), offsetHours: 0 }),
+    true,
+  );
+});
+
+Deno.test(function tuesday() {
+  assertEquals(
+    isit("Tuesday", { when: new Date("2024-10-15T00:00:00Z"), offsetHours: 0 }),
+    true,
+  );
+});
+
+Deno.test(function wednesday() {
+  assertEquals(
+    isit("Wednesday", {
+      when: new Date("2024-10-16T00:00:00Z"),
+      offsetHours: 0,
+    }),
+    true,
+  );
+});
+
+Deno.test(function thursday() {
+  assertEquals(
+    isit("Thursday", {
+      when: new Date("2024-10-17T00:00:00Z"),
+      offsetHours: 0,
+    }),
+    true,
+  );
+});
+
+Deno.test(function friday() {
+  assertEquals(
+    isit("Friday", { when: new Date("2024-10-18T00:00:00Z"), offsetHours: 0 }),
+    true,
+  );
+});
+
+Deno.test(function saturday() {
+  assertEquals(
+    isit("Saturday", {
+      when: new Date("2024-10-19T00:00:00Z"),
+      offsetHours: 0,
+    }),
+    true,
+  );
+});
+
+Deno.test(function sunday() {
+  assertEquals(
+    isit("Sunday", { when: new Date("2024-10-20T00:00:00Z"), offsetHours: 0 }),
+    true,
+  );
 });
